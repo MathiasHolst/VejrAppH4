70ed9bad0ab5fae44dda55c583928433cb6e5e37
Mathias Holst <holsthategaming@gmail.com>
2021-08-18 08:27:10 +0200

    Location weather done

--- VejrApp/VejrApp/VejrApp/MainPage.xaml.cs
+++ VejrApp/VejrApp/VejrApp/MainPage.xaml.cs
@@ -6,6 +6,10 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Essentials;
+using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
 
 namespace VejrApp
 {
@@ -15,15 +19,79 @@ namespace VejrApp
         {
             InitializeComponent();
         }
-        async void OnClick(object sender, System.EventArgs e)
-        {
-            var location = await Geolocation.GetLastKnownLocationAsync();
-            ((Button)sender).Text = $"Latitude: {location.Latitude} Longitude: {location.Longitude}";
-
-        }
         void Change_page(object sender, EventArgs e)
         {
             App.Current.MainPage = new Page1();
         }
+
+        void ApiCall(object sender, EventArgs e)
+        {
+            var json = new WebClient().DownloadString("https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=41bfac970ccb62f946ceb789cef8bb08&units=metric");
+            var rootJson = JsonConvert.DeserializeObject<Root>(json);
+        }
     }
+    public class Coord
+    {
+        public double lon { get; set; }
+        public double lat { get; set; }
+    }
+
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+    public class Main
+    {
+        public double temp { get; set; }
+        public double feels_like { get; set; }
+        public double temp_min { get; set; }
+        public double temp_max { get; set; }
+        public int pressure { get; set; }
+        public int humidity { get; set; }
+    }
+
+    public class Wind
+    {
+        public double speed { get; set; }
+        public int deg { get; set; }
+        public double gust { get; set; }
+    }
+
+    public class Clouds
+    {
+        public int all { get; set; }
+    }
+
+    public class Sys
+    {
+        public int type { get; set; }
+        public int id { get; set; }
+        public string country { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+    }
+
+    public class Root
+    {
+        public Coord coord { get; set; }
+        public List<Weather> weather { get; set; }
+        public string @base { get; set; }
+        public Main main { get; set; }
+        public int visibility { get; set; }
+        public Wind wind { get; set; }
+        public Clouds clouds { get; set; }
+        public int dt { get; set; }
+        public Sys sys { get; set; }
+        public int timezone { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int cod { get; set; }
+    }
+
+
 }
+
--- VejrApp/VejrApp/VejrApp/Page1.xaml.cs
+++ VejrApp/VejrApp/VejrApp/Page1.xaml.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Net;
+using VejrApp;
+using Newtonsoft.Json;
 
 namespace VejrApp
 {
@@ -15,6 +20,15 @@ namespace VejrApp
         public Page1()
         {
             InitializeComponent();
+            var location = Geolocation.GetLastKnownLocationAsync();
+            //var json = new WebClient().DownloadString("https://api.openweathermap.org/data/2.5/weather?q=berlin&appid=41bfac970ccb62f946ceb789cef8bb08&units=metric");
+            var json = new WebClient().DownloadString($"https://api.openweathermap.org/data/2.5/weather?lat={location.Result.Latitude}&lon={location.Result.Longitude}&appid=41bfac970ccb62f946ceb789cef8bb08&units=metric");
+            var rootJson = JsonConvert.DeserializeObject<Root>(json);
+            degrees.Text = $"Degrees: " + rootJson.main.temp.ToString();
+            humidity.Text = $"Humidity: " + rootJson.main.humidity.ToString();
+            desc.Text = $"Description: " + rootJson.weather[0].description;
+            Header.Text = rootJson.name;
+
         }
         void Front_page(object sender, EventArgs e)
         {
